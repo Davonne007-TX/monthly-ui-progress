70af4faa700f32cbe05027e30a2922b137776185
Davonne Vigil <davonnejv@gmail.com>
2025-12-26 23:37:41 -0600

    update: wonkru image size, add link to portfolio

--- src/components/KeepGoing.jsx
+++ src/components/KeepGoing.jsx
@@ -1,12 +1,17 @@
 export default function KeepGoing({ backgroundColor, motivationTxt }) {
   return (
     <section
-      className={`${backgroundColor}  flex justify-center items-center w-full py-4 mx-4 animate-fadeIn`}
+      className={`${backgroundColor} flex flex-col gap-2 justify-center items-center w-full py-4 mx-4 animate-fadeIn`}
     >
       <h2 className="text-3xl md:text-6xl font-right text-center text-black">
         {motivationTxt}
       </h2>
-      <a href="">My Portfolio</a>
+      <a
+        href="https://davonnessite.vercel.app/"
+        className="font-mono hover:scale-105 text-lg text-center md:text-xl"
+      >
+        Wanna learn more? Check out my portfolio
+      </a>
     </section>
   );
 }
